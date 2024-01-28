e43bb9f5cf745a14fd0bd46b96679e444abc8810
Dana M <147849932+dkfmedia@users.noreply.github.com>
2024-01-28 22:37:46 +0000

    tried my best

--- src/js/component/todo.jsx
+++ src/js/component/todo.jsx
@@ -1,4 +1,4 @@
-import React, {useState, UseEffect} from "react";
+import React, {useState, useEffect} from "react";
 import Home from "./home";
 import { Trash } from 'react-bootstrap-icons'
 
@@ -12,13 +12,71 @@ const Todo = () => {
         const saveTasks = (e) => {
             if (e.key === "Enter"){
                 setTasks (tasks.concat([inputValue]));
-                    setInputValue("")
-                }}
- 
-        const removeTasks = (indexToRemove) => {
-             setTasks(tasks.filter((_, currentIndex) => currentIndex !== indexToRemove));
-                 };
+                    setInputValue("");
+                    setTasks(updatedTasks);
+                    updateTasksOnServer(updatedTasks);
+                }
+            };
+
+            const removeTasks = (indexToRemove) => {
+                setTasks(tasks.filter((_, currentIndex) => currentIndex !== indexToRemove));
+                setTasks(updatedTasks);
+                updateTasksOnServer(updatedTasks);
+                };
+            
+        const updateTasksOnServer = (updatedTasks) => { 
+            fetch('https://playground.4geeks.com/apis/fake/todos/user/Dana-M', {
+                method: "PUT",
+                headers: {
+                 "Content-Type": "application/json"
+                },
+                body: JSON.stringify(updatedTasks),
+             })
+              .then(resp => {
+                  console.log(resp.ok); // Will be true if the response is successful
+                 console.log(resp.status); // The status code=200 or code=400 etc.
+                      console.log(resp.text()); // Will try to return the exact result as a string
+                      return resp.json(); // (returns promise) Will try to parse the result as JSON and return a promise that you can .then for results
+                  })
+                  .then(data => {
+                     // Here is where your code should start after the fetch finishes
+                      console.log(data); // This will print on the console the exact object received from the server
+                      setTasks(data);
+                      console.log('Tasks updated on the server:', data)
+                  })
+                .catch(error => {
+                      // Error handling
+                      console.log(error);
+                  })
+
+        }
+
+        
                 
+        useEffect (() => { 
+                 fetch('https://playground.4geeks.com/apis/fake/todos/user/Dana-M', {
+                      method: "GET",
+                      headers: {
+                       "Content-Type": "application/json"
+                      }
+                   })
+                    .then(resp => {
+                        console.log(resp.ok); // Will be true if the response is successful
+                       console.log(resp.status); // The status code=200 or code=400 etc.
+                            console.log(resp.text()); // Will try to return the exact result as a string
+                            return resp.json(); // (returns promise) Will try to parse the result as JSON and return a promise that you can .then for results
+                        })
+                        .then(data => {
+                           // Here is where your code should start after the fetch finishes
+                            console.log(data); // This will print on the console the exact object received from the server
+                            setTasks(data);
+                        })
+                      .catch(error => {
+                            // Error handling
+                            console.log(error);
+                        })
+        })
+        
     return (
         <div> 
             <ul> 
@@ -30,7 +88,8 @@ const Todo = () => {
                 ></input>
                  </li> 
                 {tasks.map((item, index) => (
-                    <li> {item}{""} 
+                   
+                     <li> {item}{""} 
                     <button onClick={() => removeTasks(index)}>
                         <Trash size={20} /> </button>
                     </li>
