4f312339ee76f21088f3f88ec4c91ee1f8e53756
Dana M <147849932+dkfmedia@users.noreply.github.com>
2024-02-07 19:02:48 +0000

    fixed my fetch request as suggested

--- src/js/component/todo.jsx
+++ src/js/component/todo.jsx
@@ -10,46 +10,38 @@ const Todo = () => {
         const [ inputValue, setInputValue ] = useState(''); 
 
         const saveTasks = (e) => {
-            if (e.key === "Enter"){
-                setTasks (tasks.concat([inputValue]));
-                    setInputValue("");
-                    setTasks(updatedTasks);
-                    updateTasksOnServer(updatedTasks);
-                }
-            };
-
-            const removeTasks = (indexToRemove) => {
-                setTasks(tasks.filter((_, currentIndex) => currentIndex !== indexToRemove));
-                setTasks(updatedTasks);
-                updateTasksOnServer(updatedTasks);
+            if (e.key === "Enter") {
+                const updatedTasks = tasks.concat([inputValue]); // Update tasks with the new input value
+                setInputValue(""); // Clear the input value
+                updateTasksOnServer(updatedTasks); // Update tasks on the server
+            }
+        };
+        
+        const removeTasks = (indexToRemove) => {
+            const updatedTasks = tasks.filter((_, currentIndex) => currentIndex !== indexToRemove); // Remove the task at the specified index
+            updateTasksOnServer(updatedTasks); // Update tasks on the server
+        };
+        
+                const updateTasksOnServer = async (updatedTasks) => {
+                    try {
+                        const resp = await fetch('https://playground.4geeks.com/apis/fake/todos/user/Dana-M', {
+                            method: "PUT",
+                            headers: {
+                                "Content-Type": "application/json"
+                            },
+                            body: JSON.stringify(updatedTasks),
+                        });
+                        if (!resp.ok) {
+                            throw new Error('Failed to update tasks on server');
+                        }
+                        const data = await resp.json();
+                        setTasks(data.map(task => task.label)); // Assuming the label property contains the task text
+                        console.log('Tasks updated on the server:', data);
+                    } catch (error) {
+                        console.log(error);
+                    }
                 };
-            
-        const updateTasksOnServer = (updatedTasks) => { 
-            fetch('https://playground.4geeks.com/apis/fake/todos/user/Dana-M', {
-                method: "PUT",
-                headers: {
-                 "Content-Type": "application/json"
-                },
-                body: JSON.stringify(updatedTasks),
-             })
-              .then(resp => {
-                  console.log(resp.ok); // Will be true if the response is successful
-                 console.log(resp.status); // The status code=200 or code=400 etc.
-                      console.log(resp.text()); // Will try to return the exact result as a string
-                      return resp.json(); // (returns promise) Will try to parse the result as JSON and return a promise that you can .then for results
-                  })
-                  .then(data => {
-                     // Here is where your code should start after the fetch finishes
-                      console.log(data); // This will print on the console the exact object received from the server
-                      setTasks(data);
-                      console.log('Tasks updated on the server:', data)
-                  })
-                .catch(error => {
-                      // Error handling
-                      console.log(error);
-                  })
-
-        }
+                
 
         
                 
@@ -89,7 +81,7 @@ const Todo = () => {
                  </li> 
                 {tasks.map((item, index) => (
                    
-                     <li> {item}{""} 
+                     <li> {item}{item.label}{""} 
                     <button onClick={() => removeTasks(index)}>
                         <Trash size={20} /> </button>
                     </li>
